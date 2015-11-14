5cf341383fc094bc09ed88a26e4d008f201916ef
Bryan Willis <willis.bryan@live.com>
2015-11-14 04:19:23 -0600

    Update load-assets.php

--- lib/load-assets.php
+++ lib/load-assets.php
@@ -8,10 +8,10 @@ remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 function bsg_enqueue_css_js() {
 
     // wp_register_style( $handle, $src, $deps, $ver, $media );
-    wp_register_style( 'bsg-combined-css', plugins_url('css/style.min.css', __DIR__), array(), '3.3.5' );
+    wp_register_style( 'bsg-combined-css', plugins_url('bootstrap/css/style.min.css', __DIR__), array(), '3.3.5' );
 
     // wp_register_script( $handle, $src, $deps, $ver, $in_footer );
-    wp_register_script( 'bsg-combined-js', plugins_url('js/javascript.min.js', __DIR__), array('jquery'), '3.3.5', true );
+    wp_register_script( 'bsg-combined-js', plugins_url('bootstrap/js/javascript.min.js', __DIR__), array('jquery'), '3.3.5', true );
 
 
     wp_enqueue_style( 'bsg-combined-css' );
